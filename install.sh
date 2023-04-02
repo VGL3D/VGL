10eb968c13b6c79aafe087205b76994eb872aadc
sohamsinh31 <sohamb2019@gmail.com>
2023-04-02 17:48:46 +0530

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,20 @@
- apt-get install libgl1-mesa-dev
- apt-get install libxtst-dev
- apt install ninja-build
- apt-get install libicu-dev
- apt install libgl1-mesa-dev
- apt install libglu1-mesa-dev
- apt-get install libxcursor-dev libxrandr-dev libxinerama-dev libxi-dev
- apt-get install libx11-dev
- apt-get install libavcodec-dev
- apt-get install libavformat-dev
- apt-get install libavfilter-dev
- apt-get install libavdevice-dev
- apt-get install libavutil-dev
- apt-get install libswresample-dev
- apt-get install libswrescale-dev
- apt-get install libswscale-dev
- apt update &&  apt upgrade
- apt install ffmpeg
-apt-get install libglfw3-dev
+ apt-get install libgl1-mesa-dev -y
+ apt-get install libxtst-dev -y
+ apt install ninja-build -y
+ apt-get install libicu-dev -y
+ apt install libgl1-mesa-dev -y
+ apt install libglu1-mesa-dev -y
+ apt-get install libxcursor-dev libxrandr-dev libxinerama-dev libxi-dev  -y
+ apt-get install libx11-dev -y
+ apt-get install libavcodec-dev -y
+ apt-get install libavformat-dev -y
+ apt-get install libavfilter-dev -y
+ apt-get install libavdevice-dev -y
+ apt-get install libavutil-dev -y
+ apt-get install libswresample-dev -y
+ apt-get install libswrescale-dev -y
+ apt-get install libswscale-dev -y
+ apt update &&  apt upgrade -y
+ apt install ffmpeg -y
+apt-get install libglfw3-dev -y
+apt-get install libglm-dev  -y
